de45738fd3356bdaf34484c76a94bb6208f2dfa2
Aditya <adityaislegend@outlook.com>
2024-07-04 23:15:32 +0530

    Full Custom Template

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,27 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import process from "process";
+import { defineConfig, loadEnv } from "vite";
 
- 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve("./src"),
+export default defineConfig(({ mode }) => {
+  // Load environment variables from .env files
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
+      },
     },
-  },
-})
+    define: {
+      // Make sure to stringify the values
+      'process.env': {
+        ...Object.keys(env).reduce((prev, next) => {
+          prev[next] = JSON.stringify(env[next]);
+          return prev;
+        }, {})
+      }
+    }
+  };
+});
